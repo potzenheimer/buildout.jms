08a03e1648a1172bcf56933ba610075879c2b2cd
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-05-15 22:34:06 +0200

    Simplify the frontend and make the view inherit from the theme template

--- src/meetshaus.jmscontent/meetshaus/jmscontent/contentpage.py
+++ src/meetshaus.jmscontent/meetshaus/jmscontent/contentpage.py
@@ -1,22 +1,11 @@
 from five import grok
 from plone.directives import dexterity, form
-
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.field import NamedBlobImage
 
 from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from meetshaus.jmscontent import MessageFactory as _
 
@@ -28,7 +17,7 @@ class IContentPage(form.Schema, IImageScaleTraversable):
     A cdocument/page type inlcuding preview images
     """
     headline = schema.TextLine(
-        title=-(u"Headline"),
+        title=_(u"Headline"),
         description=_(u"Enter an alternative headline for this page. Leave "
                       u"empty to use the standard document title"),
         required=False,
