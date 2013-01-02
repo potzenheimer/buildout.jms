edbbdddca848a3d48fd9be936ba27fdadd141dac
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-01-02 23:52:39 +0100

    Massive updates to now fully embrace bootstrap

--- src/meetshaus.jmstheme/meetshaus/jmstheme/resources/js/main.js
+++ src/meetshaus.jmstheme/meetshaus/jmstheme/resources/js/main.js
@@ -2,17 +2,24 @@
 /*global jQuery:false, document:false, window:false, location:false */
 
 (function ($) {
-$(document).ready(function () {
-    if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {
-        // it's not realistic to think we can deal with all the bugs
-        // of IE 6 and lower. Fortunately, all this is just progressive
-        // enhancement.
-        return;
-    }
-    $('#banner-scrollable').scrollable({
-        circular: true
-    }).autoscroll(6000).navigator();
-
-    $("a[rel=prettyPhoto]").prettyPhoto();
-});
+    $(document).ready(function () {
+        if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {
+            // it's not realistic to think we can deal with all the bugs
+            // of IE 6 and lower. Fortunately, all this is just progressive
+            // enhancement.
+            return;
+        }
+        $('#banner-scrollable').scrollable({
+            circular: true
+        }).autoscroll(6000).navigator();
+    
+        $("a[rel=prettyPhoto]").prettyPhoto();
+    
+        var $sameHeightDivs = $("div[data-appui='leveled']");
+        var maxHeight = 0;
+        $sameHeightDivs.each(function () {
+            maxHeight = Math.max(maxHeight, $(this).outerHeight());
+        });
+        $sameHeightDivs.css({ height: maxHeight + 'px' });
+    });
 }(jQuery));
